4c9ae0b4b916a5e7f1e22bfb18829f1c327a1785
Adroaldo <hygino@msn.com>
2021-06-24 10:20:08 -0300

    Primeiros testes em JavaScript

---
+++ aula11/ex08.js
@@ -0,0 +1,8 @@
+var v = 12//60.5
+console.log(`A velocidade do seu carro é ${v} Km/h`)
+
+if (v > 60) {
+    console.log(`Voce ultrapassou o limite de velocidade`)
+}
+
+console.log("Use cinto de segurança")
